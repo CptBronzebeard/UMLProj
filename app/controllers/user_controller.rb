064a2808dc45f91b16d5012d17b0b8d886ea45e2
CptBronzebeard <omg4itepok@gmail.com>
2019-03-15 01:58:15 +0300

    github controllers

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,34 @@
 class UserController < ApplicationController
+# ----- add these lines here: -----
+
   def new
+    @user = User.new
   end
+
   def create
-    user = User.new(name: params[:name], email: params[:email], password_digest: params[:password])
-    if user.save
-      session[:user_id]=user.id
-      flash[:success]="Success"
-      redirect_to '/'
+    @user = User.new(user_params)
+
+    # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
+    @user.email.downcase!
+
+    if @user.save
+      # If user saves in the db successfully:
+      flash[:notice] = "Account created successfully!"
+      redirect_to '/login'
     else
-      flash[:warning]="Warn"
+      # If user fails model validation - probably a bad password or duplicate email:
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
       render :new
     end
-end
+  end
+
+private
+
+  def user_params
+    # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
+    # that can be submitted by a form to the user model #=> require(:user)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+# ----- end of added lines -----
 end
